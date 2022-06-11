d49863cb98d3d3038bcc2526bfb5b2fe5b474428
matumoto1234 <matumotosizuoka@gmail.com>
2022-06-11 19:43:57 +0900

    fix js

--- ex01/ex01.js
+++ ex01/ex01.js
@@ -1,7 +1,7 @@
 const onClick = () => {
   const inputElement = document.getElementsByClassName("ex01Item-input")[0]
-  alert(inputElement.textContent)
-  inputElement.textContent = ""
+  alert(inputElement.value)
+  inputElement.value = ""
 }
 
 const buttonElement = document.getElementsByClassName("ex01Item-button")[0]
