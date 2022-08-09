64ab305077cbda6535b69353b7762c3bfd15cae9
matumoto1234 <matumotosizuoka@gmail.com>
2022-08-09 19:52:28 +0900

    remove some

--- ex01/ex01.js
+++
@@ -1,8 +0,0 @@
-const onClick = () => {
-  const inputElement = document.getElementsByClassName("ex01Item-input")[0]
-  alert(inputElement.value)
-  inputElement.value = ""
-}
-
-const buttonElement = document.getElementsByClassName("ex01Item-button")[0]
-buttonElement.addEventListener('click', onClick)
